e7e154ceafe607b3a4b2cf8c37c6b19f40edd517
alexr <reitsak.alex@gmail.com>
2024-02-15 13:01:08 +0200

    week2

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//int UserAge = Int32.Parse (Console.ReadLine()); //"13" - heap, 13 - stack 
+
+Console.WriteLine($"Parse result {isAgeNumber}, user is {userAgeNum} years old.");
+
+if (isAgeNumber == true)
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagaram!");
+     }
+    else 
+    { Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Alex!");
+// 
+Console.WriteLine("Please select your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine());//
+
+Console.WriteLine("Please enter your last name:");
+
+    string UserLastName = Console.ReadLine();
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {UserLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {UserLastName}!");
+}
+else 
+{
+    Console.WriteLine($"Welcome, {UserLastName}!");
+}
